45e5e09e73f7d0e286501efe2cf9587f8968d5d7
woojeans1204 <woojeans1204@gmail.com>
2024-10-03 11:52:07 +0900

    html 완성 (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,28 @@ export const App = () => {
           <img src="" />
           <h1 className={styles.title}>TimeTable</h1>
         </div>
-        <button className={styles.loginButton}>로그인</button>
+        <div>
+          <div>
+            <button className={styles.loginButton}>로그인</button>
+            <button className={styles.signUpButton}>회원가입</button>
+          </div>
+          <div>
+            <div>
+              <div>
+                <hr />
+                SNS 계정으로 계속하기
+                <hr />
+              </div>
+
+              <div>
+                <img src="" /> {/* 카톡 구글 페북 애플 아이콘 */}
+                <img src="" />
+                <img src="" />
+                <img src="" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
