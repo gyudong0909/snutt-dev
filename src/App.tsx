e5867a28031ebddb4db1227dc320d2b01de260d3
woojeans1204 <woojeans1204@gmail.com>
2024-10-09 13:25:11 +0900

    병합 충돌 해결

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,32 @@ import './reset.css';
 import { useState } from 'react';
 
 import Home from './Home';
-import Landing from './Landing';
+import Landing from './Landing.tsx';
 import Login from './Login';
-
 export const App = () => {
   const [currentPage, setCurrentPage] = useState<'home' | 'login' | 'landing'>(
     'home',
   );
-
+  const [nickname, setNickname] = useState<{
+    nickname: string;
+    tag: string;
+  }>();
   const handleLoginButton = () => {
     setCurrentPage('login');
   };
-
   const handleLoginSuccess = () => {
     setCurrentPage('landing');
   };
-
   return (
     <>
       {currentPage === 'home' && <Home onLoginButton={handleLoginButton} />}
-      {currentPage === 'login' && <Login onLoginSuccess={handleLoginSuccess} />}
-      {currentPage === 'landing' && <Landing nickname="박규동#0000" />}
+      {currentPage === 'login' && (
+        <Login setNickname={setNickname} onLoginSuccess={handleLoginSuccess} />
+      )}
+      {currentPage === 'landing' && nickname !== undefined && (
+        <Landing nickname={`${nickname.nickname}#${nickname.tag}`} />
+      )}
     </>
   );
 };
+export default App;
