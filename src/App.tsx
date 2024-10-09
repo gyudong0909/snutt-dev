e6ba4b1c947ad294e33185e7c7360ca0d29ffa14
박규동 <parkgyudong@baggyudong-ui-MacBookPro.local>
2024-10-09 12:44:54 +0900

    api구현을 제외한 거의 전부

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import './reset.css';
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import Home from './Home';
+import Landing from './Landing';
 import Login from './Login';
 
 export const App = () => {
-  const [isLoginPage, setIsLoginPage] = useState(false);
-
-  useEffect(() => {
-    setIsLoginPage(false);
-  }, []);
+  const [currentPage, setCurrentPage] = useState<'home' | 'login' | 'landing'>(
+    'home',
+  );
 
   const handleLoginButton = () => {
-    setIsLoginPage(true);
+    setCurrentPage('login');
   };
 
-  return isLoginPage ? <Login /> : <Home onLoginButton={handleLoginButton} />;
+  const handleLoginSuccess = () => {
+    setCurrentPage('landing');
+  };
+
+  return (
+    <>
+      {currentPage === 'home' && <Home onLoginButton={handleLoginButton} />}
+      {currentPage === 'login' && <Login onLoginSuccess={handleLoginSuccess} />}
+      {currentPage === 'landing' && <Landing nickname="박규동#0000" />}
+    </>
+  );
 };
+
+export default App;
--- src/Login.tsx
+++ src/Login.tsx
@@ -2,24 +2,45 @@ import './reset.css';
 
 import styles from './Login.module.css';
 
-const Login = () => {
+interface LoginProps {
+  onLoginSuccess: () => void;
+}
+
+const Login = ({ onLoginSuccess }: LoginProps) => {
+  const handleLogin = () => {
+    // 로그인 로직 수행 후 성공 시 호출
+    onLoginSuccess();
+  };
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.content}>
-        {' '}
         {/* 로그인 페이지 */}
         <div>
+          <label className={styles.title}>로그인</label>
           <div className={styles.input}>
             <label>아이디</label>
-            <input type="text" placeholder="아이디를 입력하세요" />
+            <input
+              type="text"
+              id="id"
+              placeholder="아이디를 입력하세요"
+              className={styles.box}
+            />
           </div>
           <div className={styles.input}>
             <label>비밀번호</label>
-            <input type="text" placeholder="비밀번호를 입력하세요" />
+            <input
+              type="password"
+              id="pw"
+              placeholder="비밀번호를 입력하세요"
+              className={styles.box}
+            />
           </div>
-          <div>아이디 찾기 | 비밀번호 재설정</div>
+          <div className={styles.extra}>아이디 찾기 | 비밀번호 재설정</div>
         </div>
-        <button>로그인</button>
+        <button onClick={handleLogin} className={styles.bottom}>
+          로그인
+        </button>
       </div>
     </div>
   );
