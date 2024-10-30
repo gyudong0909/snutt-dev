e99053cef5482a2d915b873983b14ddfca4a2cd9
woojeans1204 <woojeans1204@gmail.com>
2024-10-30 20:02:17 +0900

    PR9 리뷰를 바탕으로 개선 주요 개선점: fetch의 분리, Login에서 Token을 넘겨주어, 직접 Landing에서 nickname을 fetch

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -1,15 +1,59 @@
 import './reset.css';
 
+import { useEffect,useState } from 'react';
+
 import styles from './Landing.module.css';
 
 interface LandingProps {
-  nickname: string;
+  token: string;
 }
 
-const Landing = ({ nickname }: LandingProps) => {
+type MeResponse = {
+  id: string;
+  isAdmin: boolean;
+  regDate: string;
+  notificationCheckedAt: string;
+  email: string;
+  localId: string;
+  fbName: string;
+  nickname: { nickname: string; tag: string };
+};
+
+const Landing = ({ token }: LandingProps) => {
+  const [meData, setMeData] = useState<MeResponse>();
+  const [name, setName] = useState<string>('');
+  useEffect(() => {
+    const fetchMeRequest = async () => {
+      const response = await fetch(
+        `https://wafflestudio-seminar-2024-snutt-redirect.vercel.app/v1/users/me`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'x-access-token': token,
+          },
+        },
+      );
+      return response;
+    };
+    const requestMe = async () => {
+      const meResponse = await fetchMeRequest();
+      if (meResponse.type === 'error') {
+        alert('meRequest fetch 오류 발생');
+      }
+      const data = (await meResponse.json()) as MeResponse;
+      setMeData(data);
+    };
+    requestMe().catch((e: unknown) => {
+      alert(e);
+    });
+    if (meData !== undefined)
+      setName(`${meData.nickname.nickname}#${meData.nickname.tag}`);
+  }, [token, setName, meData]);
+
   return (
     <div className={styles.wrapper}>
-      <h1 className={styles.body}>{nickname}</h1>
+      <h1 className={styles.body}>{name}</h1>
     </div>
   );
 };
