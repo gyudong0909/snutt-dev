91858e98ee890a70db44267e187b5052a2e1718d
박규동 <parkgyudong@baggyudong-ui-MacBookPro.local>
2024-11-10 23:41:21 +0900

    마이페이지 이쁘게 변경

--- src/Mypage.tsx
+++ src/Mypage.tsx
@@ -23,7 +23,6 @@ type MeResponse = {
 
 const MyPage = ({ token, onLogout }: MyPageProps) => {
   const [meData, setMeData] = useState<MeResponse | null>(null);
-  const [name, setName] = useState<string>('');
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -69,19 +68,90 @@ const MyPage = ({ token, onLogout }: MyPageProps) => {
     void fetchMeData();
   }, [token, navigate]);
 
-  useEffect(() => {
-    if (meData != null) {
-      setName(`${meData.nickname.nickname}#${meData.nickname.tag}`);
-    }
-  }, [meData]);
-
   return (
-    <div className={styles.wrapper}>
-      <h1>마이페이지</h1>
-      <h2 className={styles.body}>{name}</h2>
-      <button className={styles.button} onClick={onLogout}>
-        로그아웃
-      </button>
+    <div className={styles.container}>
+      <div className={styles.upperBar}>
+        <div className={styles.sideSection}></div>
+        <div className={styles.middleSection}>마이페이지</div>
+        <div className={styles.sideSection}></div>
+      </div>
+      <div className={styles.content}>
+        <button
+          className={styles.button}
+          onClick={() => {
+            navigate('/account');
+          }}
+          style={{ marginTop: '40px' }}
+        >
+          <div className={styles.buttonLeft}>내 계정</div>
+          <div className={styles.buttonRight}>
+            {meData != null
+              ? `${meData.nickname.nickname}#${meData.nickname.tag} `
+              : 'Error'}
+            &nbsp; {'〉'}
+          </div>
+        </button>
+        <div className={styles.subtitle}></div>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>화면 모드</div>
+          <div className={styles.buttonRight}>자동 &nbsp; {'〉'}</div>
+        </button>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>시간표 설정</div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>시간표 테마</div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+
+        <div className={styles.subtitle}></div>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>빈자리 알림</div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+
+        <div className={styles.subtitle}></div>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>버전 정보</div>
+          <div className={styles.buttonRight}>3.8.2 &nbsp;</div>
+        </button>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>개발자 정보</div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+
+        <div className={styles.subtitle}></div>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>개발자 괴롭히기</div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+
+        <div className={styles.subtitle}></div>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>오픈소스 라이선스</div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>서비스 약관</div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+        <button className={styles.button}>
+          <div className={styles.buttonLeft}>개인정보처리방침</div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+
+        <div className={styles.subtitle}></div>
+        <button className={styles.button} onClick={onLogout}>
+          <div
+            className={styles.buttonLeft}
+            style={{ color: 'red', fontWeight: 'bold' }}
+          >
+            로그아웃
+          </div>
+          <div className={styles.buttonRight}>&nbsp; {'〉'}</div>
+        </button>
+      </div>
     </div>
   );
 };
